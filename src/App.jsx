cd3d8bc7397b65325113fedc35f42de92c5603c5
onopod <onopod@google.com>
2024-11-19 21:08:55 +0900

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Main from "./components/Main.jsx";
+import Sidebar from "./components/Sidebar.jsx";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Sidebar />
+      <Main />
+    </div>
+  )
+}
+
+export default App
